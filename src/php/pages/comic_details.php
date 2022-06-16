c32d1d92162a8b4887af9fa243ae01305e24a155
Raymon Roos <raymon.roos@student.rocva.nl>
2022-06-16 16:32:00 +0200

    Fixed header for webshop-comic-page

--- src/php/pages/comic_details.php
+++ src/php/pages/comic_details.php
@@ -12,15 +12,16 @@ $comic = findComicByID($_GET['comicID']);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="img/index.ico">
     <title>Comic details</title>
-    <link rel="stylesheet" href="../../../dist/output.css">
+    <link rel="stylesheet" href="/dist/output.css">
 </head>
 
 
 <body class="bg-old_paper-100">
-<div class="w-9/12 min-w-fit mx-auto p-2 bg-old_paper-200 ">
+<div class="w-9/12 mx-auto p-2 bg-old_paper-200 ">
 
-<?php require_once('../../components/header.html'); ?>
+<?php require_once('../../components/header_shop.html'); ?>
 
     <article class="p-28">
         <section class="text-justify font-serif">
