9366ab56a9ade0c6ff1d1a10fc37f02415afb808
Raymon Roos <raymon.roos@student.rocva.nl>
2022-07-05 14:56:07 +0200

    Slightly tweaked carousel indicators
    (cherry picked from commit 6f3e9036ab066cbc9b2d0d801b21cdcaabf7d2fe)

--- src/components/carousel.php
+++ src/components/carousel.php
@@ -1,7 +1,8 @@
 <section class="w-5/12 h-1/3">
-    <div id="carousel" class="carousel slide carousel-fade carousel-dark w-full h-full p-3 px-8 relative border-8 border-modern_white_smoke border-opacity-25 bg-modern_white_smoke bg-opacity-80" data-bs-ride="carousel">
+    <div id="carousel" data-bs-ride="carousel"
+        class="carousel slide carousel-fade carousel-dark w-full h-full p-3 px-8 relative border-8 border-modern_white_smoke border-opacity-25 bg-modern_white_smoke bg-opacity-80">
         <h1 class="text-5xl font-bebas drop-shadow-xl artist_title text-center">Artists of the day</h1>
-        <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4 ">
+        <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-1 ">
             <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
             <?php for ($i = 1; $i < count($randArticles); $i++) { ?>
                 <button type="button" data-bs-target="#carousel" data-bs-slide-to="<?= $i ?>" aria-label="Slide <?= $i ?>"></button>
@@ -11,7 +12,7 @@
         <div class="carousel-inner w-full h-full relative overflow-hidden object-contain object-center m-1">
         <?php foreach ($randArticles as $article) { ?>
             <div class="carousel-item relative float-left w-full min-h-fit h-full max-h-[700px] object-contain object-center">
-                <img src="../../../image/<?= $article['imgofn'] ?>" class="object-contain object-center w-full h-full max-h-full" alt="" />
+                <img src="https://www.lambiek.net/artists/image/<?= $article['imgofn'] ?>" class="object-contain object-center w-full h-full max-h-full" alt="" />
                 <div class="carousel-caption hidden md:block absolute text-center ">
                     <a href="artist_details.php?artist=<?= $article['id'] ?>">
                         <p class="bg-modern_light_blue hover:bg-modern_blue text-modern_white_smoke font-bungee py-3 px-6 rounded-xl"><?= $article['name'] ?></p>
