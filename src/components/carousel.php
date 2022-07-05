03900da7b417c3b95b1704ecd0667c1151322586
Raymon Roos <raymon.roos@student.rocva.nl>
2022-07-05 14:56:07 +0200

    Tweaked carousel
    Made next and prev buttons more visible
    
    Slight refactors
    
    (cherry picked from commit 121af3fee92c7f7fec22da89a90429b8222d09d9)

--- src/components/carousel.php
+++ src/components/carousel.php
@@ -1,5 +1,5 @@
-<section class="w-2/5 h-1/3">
-    <div id="carousel" class="carousel slide carousel-fade carousel-dark w-full h-full relative border-8 border-modern_white_smoke border-opacity-25 bg-modern_white_smoke bg-opacity-80" data-bs-ride="carousel">
+<section class="w-5/12 h-1/3">
+    <div id="carousel" class="carousel slide carousel-fade carousel-dark w-full h-full p-3 px-8 relative border-8 border-modern_white_smoke border-opacity-25 bg-modern_white_smoke bg-opacity-80" data-bs-ride="carousel">
         <h1 class="text-5xl font-bebas drop-shadow-xl artist_title text-center">Artists of the day</h1>
         <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4 ">
             <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -8,7 +8,7 @@
             <?php } ?>
         </div>
 
-        <div class="carousel-inner w-full h-full relative overflow-hidden object-contain object-center">
+        <div class="carousel-inner w-full h-full relative overflow-hidden object-contain object-center m-1">
         <?php foreach ($randArticles as $article) { ?>
             <div class="carousel-item relative float-left w-full min-h-fit h-full max-h-[700px] object-contain object-center">
                 <img src="../../../image/<?= $article['imgofn'] ?>" class="object-contain object-center w-full h-full max-h-full" alt="" />
@@ -21,12 +21,14 @@
         <?php } ?>
         </div>
 
-        <button class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0 text-comic_blue hover:text-modern_blue" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+        <button class="carousel-control-prev absolute top-0 bottom-0 left-[-5%] flex items-center justify-center p-0 text-center border-0 outline-none no-underline"
+        type="button" data-bs-target="#carousel" data-bs-slide="prev">
             <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
 
-        <button class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0" type="button" data-bs-target="#carousel" data-bs-slide="next">
+        <button class="carousel-control-next absolute top-0 bottom-0 right-[-5%] flex items-center justify-center p-0 text-center border-0 outline-none no-underline"
+        type="button" data-bs-target="#carousel" data-bs-slide="next">
             <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
